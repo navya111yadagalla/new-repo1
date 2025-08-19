5d14aafe134e4a1e6f3c3d18d954eb052c86382b
K102eethi <pallerlakeerthireddy222@gmail.com>
2025-08-19 18:05:35 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,10 +18,7 @@ public class App {
 
     static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-PocDemo
-            String response = "Hello Keerthi,  CICD Pipeline Project Completed";
             String response = "Hello Keerthi,  I am from Java CI/CD! This is for sample";
- main
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
